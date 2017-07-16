c6270ab6107a6ea692a4e2c441ffdc682bafde12
王二小 <lgl48128244@126.com>
2017-07-16 23:36:33 +0800

    update project home

--- spring_data_jpa/src/main/java/com/springdata/repository/UserRepository.java
+++ spring_data_jpa/src/main/java/com/springdata/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.springdata.repository;
 
 import com.springdata.entity.Tb_user;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import javax.persistence.QueryHint;
 
 /**
  * Created by Administrator on 2017/7/15.
@@ -10,5 +13,10 @@ public interface UserRepository extends JpaRepository<Tb_user,Long> {
 
     Tb_user save(Tb_user tb_user);
 
+    @QueryHints(value = {
+            @QueryHint(name = "org.hibernate.cacheable", value = "true"),
+            @QueryHint(name = "org.hibernate.cacheMode", value = "NORMAL"),
+            @QueryHint(name = "org.hibernate.cacheRegion", value = "entityCache")
+    })
     Tb_user findById(Long id);
 }
